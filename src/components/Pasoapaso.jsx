c28a92d5c66bc5b53c8e591932a73f6130b49111
vivi_soto <anavivi-24@hotmail.com>
2020-11-21 00:54:37 -0500

    principio del paso a paso

--- src/components/Pasoapaso.jsx
+++ src/components/Pasoapaso.jsx
@@ -2,23 +2,44 @@ import React from 'react';
 import '../assets/styles/components/pasoapaso.scss';
 
 const Pasoapaso = () => (
-  <div className='pasos'>
-    <div className='stepwizard'>
-      <div className='stepwizard-row setup-panel'>
-        <div className='stepwizard-step'>
-          <a href='#step-1' type='button' className='btn btn-primary btn-circle'>1</a>
-          <p>Step 1</p>
+  <div className='root'>
+    <form action='' className='pasos'>
+      <div className='pasos__header'>
+        <ul className='barra'>
+          <li className='barra__opcion active' id='cesta'>Cesta</li>
+          <li className='barra__opcion' id='pago'>Pago</li>
+          <li className='barra__opcion' id='listo'>Listo</li>
+        </ul>
+      </div>
+      <div className='pasos__body'>
+        <div className='paso active to-left' id='cesta'>
+          <h2>Tu cesta</h2>
+          <p>componentes correspondientes a cesta</p>
+          <input type='email' placeholder='correo' />
+          <div className='pasos__body--boton'>
+            <button type='button' className='boton__paso--next' data-to-step='pago' data-step='cesta'>siguiente</button>
+          </div>
         </div>
-        <div className='stepwizard-step'>
-          <a href='#step-2' type='button' className='btn btn-default btn-circle' disabled='disabled'>2</a>
-          <p>Step 2</p>
+
+        <div className='paso' id='pago'>
+          <h2>Elige tu forma de pago</h2>
+          <p>componentes correspondientes a pago</p>
+          <input type='text' placeholder='nombre' />
+          <div className='pasos__body--boton'>
+            <button type='button' className='boton__paso--next' data-to-step='listo' data-step='pago'>continuar</button>
+          </div>
         </div>
-        <div className='stepwizard-step'>
-          <a href='#step-3' type='button' className='btn btn-default btn-circle' disabled='disabled'>3</a>
-          <p>Step 3</p>
+
+        <div className='paso' id='listo'>
+          <h2>Pago realizado</h2>
+          <p>componentes correspondientes a pago</p>
+          <input type='text' placeholder='apellido' />
+          <div className='pasos__body--boton'>
+            <button type='submit' className='boton__paso--next' data-to-step='listo' data-step='pago'>continuar</button>
+          </div>
         </div>
       </div>
-    </div>
+    </form>
   </div>
 );
 
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
-import Beneficio from '../components/Beneficio';
-import Ups from '../components/Ups';
-import Bono from '../components/Bono';
-import TarjetaCredito from '../components/TarjetaCredito';
-import Pse from '../components/Pse';
-import Vehiculo from '../components/Vehiculo';
-import Descubrir from '../components/Descubrir';
+
 import Pasoapaso from '../components/Pasoapaso';
 
 const App = () => (
   <div className='App'>
-    <Header />
     <Pasoapaso />
-    <Descubrir />
-    <Vehiculo />
-    <Beneficio />
-    <Ups />
-    <Bono />
-    <TarjetaCredito />
-    <Pse />
-    <Footer />
   </div>
 );
 
